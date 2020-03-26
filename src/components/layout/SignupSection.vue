d876316fd4414701d05e5558d6346d46f9e49129
Cecilia E.T. Söderblom <ceciliasoderblom@Cecilias-MacBook-Pro-2.local>
2020-03-26 10:03:24 +0100

    Signup section added as component

---
+++ src/components/layout/SignupSection.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- SIGN UP SECTION -->
+  <section class="signup-section">
+    <div class="signup-container">
+      <div class="signup-text">
+        <h2>Sign up</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laborum perspiciatis soluta similique ex.</p>
+      </div>
+      <div class="signup-form">
+        <form>
+          <label for="nameLabel">Name</label>
+          <input type="text" placeholder="Name" name="nameTxt" />
+          <label for="emailLabel">E-mail</label>
+          <input type="text" placeholder="E-mail" name="emailTxt" />
+          <button class="btn-goToSignup">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.signup-section {
+  background-color: #d3cecb;
+  padding: 20px;
+}
+.signup-container {
+  width: 800px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.signup-text {
+  width: 350px;
+  color: black;
+  margin: auto;
+  line-height: 1.5;
+  font-size: 18px;
+}
+.signup-text > h2 {
+  text-align: left;
+  margin-top: 0;
+  font-size: 35px;
+}
+.signup-form {
+  margin: auto;
+  color: black;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: 18px;
+}
+.signup-form > form {
+  display: grid;
+}
+label {
+  text-align: left;
+}
+input[type="text"] {
+  width: 300px;
+  padding: 12px 20px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+}
+</style>
+
+<script>
+export default {
+  name: "SignupSection.vue"
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,23 +20,8 @@
     </div>
 
     <!-- SIGN UP SECTION -->
-    <section class="signup-section">
-      <div class="signup-container">
-        <div class="signup-text">
-          <h2>Sign up</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laborum perspiciatis soluta similique ex.</p>
-        </div>
-        <div class="signup-form">
-          <form>
-            <label for="nameLabel">Name</label>
-            <input type="text" placeholder="Name" name="nameTxt" />
-            <label for="emailLabel">E-mail</label>
-            <input type="text" placeholder="E-mail" name="emailTxt" />
-            <button class="btn-goToSignup">Sign Up</button>
-          </form>
-        </div>
-      </div>
-    </section>
+
+    <SignupSection></SignupSection>
 
     <!-- BOTTOM SECTION -->
     <div class="bottom-container">
@@ -122,49 +107,7 @@ p {
 }
 
 // SIGN UP SECTION
-.signup-section {
-  background-color: #d3cecb;
-  padding: 20px;
-}
-.signup-container {
-  width: 800px;
-  margin: auto;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-}
-.signup-text {
-  width: 350px;
-  color: black;
-  margin: auto;
-  line-height: 1.5;
-  font-size: 20px;
-}
-.signup-text > h2 {
-  text-align: left;
-  margin-top: 0;
-  font-size: 35px;
-}
-.signup-form {
-  margin: auto;
-  color: black;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  font-size: 20px;
-}
-.signup-form > form {
-  display: grid;
-}
-label {
-  text-align: left;
-}
-input[type="text"] {
-  width: 300px;
-  padding: 12px 20px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-bottom: 10px;
-}
+
 // BOTTOM SECTION
 .bottom-container {
   top: 0;
@@ -191,8 +134,10 @@ input[type="text"] {
 }
 </style>
 <script>
+import SignupSection from "@/components/layout/SignupSection";
 export default {
   name: "Home",
+  components: { SignupSection },
   data: function() {
     return {
       apiMessage: "",
@@ -224,7 +169,6 @@ export default {
       ]
     };
   },
-  components: {},
   created: function() {
     this.getMessageFromApi();
   },
