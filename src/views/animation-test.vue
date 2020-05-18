53e29b928e07338cd2566a99d61e9294262bba0d
milleprintzlau <milliewienberg@gmail.com>
2020-05-18 10:21:29 +0200

    Walking Monster

--- src/views/animation-test.vue
+++ src/views/animation-test.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="test">
-        {{apiMessage}}
-        
-       <div class="walking-guy"></div>
-          <div class="contatiner">
-            <div class="quiz">
-            <p>Here is a question</p>
-            <button v-on:click="correct" >Answer 1</button>
-            <button v-on:click="wrong">Answer 2</button>
-            </div>
-            <div class="mood">
-              <img src="../assets/svg/happy.svg" alt="" id="happy">
-              <img src="../assets/svg/angry.svg" alt="" id="angry">
-            </div>
-          </div>
+  <div class="test">
+    {{apiMessage}}
+    <div class="walking-guy"></div>
+    <div class="contatiner">
+      <div class="quiz">
+        <p>Here is a question</p>
+        <button v-on:click="correct">Answer 1</button>
+        <button v-on:click="wrong">Answer 2</button>
+      </div>
+      <div class="mood">
+        <img src="../assets/svg/happy.svg" alt id="happy" />
+        <img src="../assets/svg/angry.svg" alt id="angry" />
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss">
-.contatiner{
+.contatiner {
   width: 500px;
   margin: auto;
   display: flex;
 }
 
-.mood{
+.mood {
   width: 100px;
   height: auto;
   position: relative;
-
 }
 .mood > img {
   width: 100%;
@@ -36,27 +34,24 @@
   display: none;
 }
 
-.walking-guy{
-  width: 401.5px;
+.walking-guy {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 401px;
   height: 401px;
-  background: url("../assets/svg/walk-4.png");
-  animation: walk 1s steps(7) infinite , forward 5s linear  1 ;
-
+  background: url("../assets/svg/walk-2.png");
+  animation: walk 0.8s steps(7);
 }
 
-@keyframes walk{
-  0%{
+@keyframes walk {
+  0% {
     background-position: 0px;
-  } 100%{
-    background-position: 2811px;
+    right: -150px;
   }
-}
-
-@keyframes forward {
-  0%{
-    transform: translateX(1200px);
-  } 100% {
-    transform: translateX(0px);
+  100% {
+    background-position: 2810px;
+    right: 10px;
   }
 }
 </style>
@@ -70,15 +65,14 @@ export default {
     };
   },
   methods: {
-    correct: function (){
-      document.getElementById("happy").style.display="block";
-      document.getElementById("angry").style.display="none";
+    correct: function() {
+      document.getElementById("happy").style.display = "block";
+      document.getElementById("angry").style.display = "none";
     },
-    wrong: function (){
-      document.getElementById("angry").style.display="block";
-      document.getElementById("happy").style.display="none";
+    wrong: function() {
+      document.getElementById("angry").style.display = "block";
+      document.getElementById("happy").style.display = "none";
     }
-
   }
 };
 </script>
